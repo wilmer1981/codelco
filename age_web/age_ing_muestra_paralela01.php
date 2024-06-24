60baadf0a7c8b3de7e976ab7df397a9952044ebb
Wilmer Saldana <wilmer_1981@hotmail.com>
2024-06-24 16:36:40 -0500

    update Asignacion Muestra Paralela

--- age_web/age_ing_muestra_paralela01.php
+++ age_web/age_ing_muestra_paralela01.php
@@ -3,19 +3,24 @@
 	include("../principal/conectar_principal.php");
 	$Proceso      = isset($_REQUEST["Proceso"])?$_REQUEST["Proceso"]:"";
 	$Valores      = isset($_REQUEST["Valores"])?$_REQUEST["Valores"]:"";
+	$SubProducto  = isset($_REQUEST["SubProducto"])?$_REQUEST["SubProducto"]:"";
+	$Mes        = isset($_REQUEST["Mes"])?$_REQUEST["Mes"]:date("m");
+	$Ano        = isset($_REQUEST["Ano"])?$_REQUEST["Ano"]:date("Y");
 
 	switch ($Proceso)
 	{	
 		case "G":
 			$Datos=explode("//",$Valores);
+			//var_dump($Datos);
+			//exit();
 			foreach($Datos as $k => $v)
 			{
 				$Datos2=explode("~~",$v);
 				$Actualizar = "UPDATE age_web.lotes set muestra_paralela='$Datos2[0]' where lote = '$Datos2[1]' ";
 				mysqli_query($link, $Actualizar);
-				//echo $Actualizar."<br>";
+				echo $Actualizar."<br>";
 			}
-			header("location:age_ing_muestra_paralela.php?Mostrar=S&SubProducto=".$SubProducto."&Mes=".$Mes."&Ano=".$Ano);
+			//header("location:age_ing_muestra_paralela.php?Mostrar=S&SubProducto=".$SubProducto."&Mes=".$Mes."&Ano=".$Ano);
 			break;
 	}
 ?>	
